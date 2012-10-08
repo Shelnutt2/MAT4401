1b9658471016f76a0ff5b0d8a2ce435988fc2171
Seth Shelnutt <shelnutt2@gmail.com>
2012-10-07 22:24:03 -0400

    Finished cosine, local and lsa. Clean up needed

--- project_1.py
+++ project_1.py
@@ -3,7 +3,7 @@
 #MAT4401
 
 #All code is released under the terms of the gpl v3 or later.
-import argparse, math, sympy
+import argparse, math, sympy, time
 import numpy as np
 from matplotlib import pyplot
 import easygui as eg
@@ -17,23 +17,29 @@ parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.8')
 args = parser.parse_args()
 
 def midpoint():
-   a = input('Please enter the lower bound of the interval: ')
-   b = input('Please enter the upper bound of the interval: ')
-   print("Please click on the point you want to choose for intersection")
-   a = np.float(a)
-   b = np.float(b)
-   found = False
-   i = 0
-   fa = np.exp(-a/5) - np.sin(a)
-   fb = np.exp(-b/5) - np.sin(b)
+   while(True):
+      a = input('Please enter the lower bound of the interval: ')
+      b = input('Please enter the upper bound of the interval: ')
+      a = np.float(a)
+      b = np.float(b)
+      found = False
+      i = 0
+      fa = np.exp(-a/5) - np.sin(a)
+      fb = np.exp(-b/5) - np.sin(b)
+      if fa*fb < 0:
+         False
+         break
+      else:
+         print "There is more than one or no sign change, please choose a different interval"
    c = 0.0
    fc = 0.0
+   start = time.time()
    while(found == False):
       i = i+1
       c = (a+b)/2
-      #print("x = " + str(c))
+      print("x = " + str(c))
       fc = np.exp(-c/5) - np.sin(c)
-      #print("y = " + str(fc))
+      print("y = " + str(fc))
       if abs(fc)<10**-7:
          print("Done")
          found = True
@@ -43,13 +49,15 @@ def midpoint():
       elif fa*fc<0:
          b = c
          fb = np.exp((-b/5)) - np.sin(b)
+   end = time.time()
    print("The zero is: " + str(c))
-   print("It took " + str(i) + " interations.")
+   print("It took " + str(i) + " interations and " + str(end-start) + " seconds.")
    return [c,i]
 
 def newtons():
    a = input('Please enter a starting value: ')
    x = sympy.Symbol('x')
+   start = time.time()
    y = sympy.exp((-x/5)) - sympy.sin(x)
    f= sympy.lambdify(x, y, 'numpy')
    yprime = y.diff(x)
@@ -66,8 +74,9 @@ def newtons():
          found = True
       else:
          a = xk
+   end = time.time()
    print("The zero is: " + str(xk))
-   print("It took " + str(i) + " interations.")
+   print("It took " + str(i) + " interations and " + str(end-start) + " seconds.")
 
 def visual():
    def onclick(event):
@@ -79,6 +88,7 @@ def visual():
    
    a = input('Please enter the lower bound of the interval to graph: ')
    b = input('Please enter the upper bound of the interval to graph: ')
+   print("Please click on the point you want to choose for intersection")
    a = np.float(a)
    b = np.float(b)
    x1 = np.arange(a,b,.01)
@@ -111,7 +121,9 @@ def lagrange():
    points=[]
    for i in np.arange(-5.,5.,(10./(n+1))):
       points.append([i,1./(1.+i**2.)])
+   points.append([5.,1./(1.+5.**2.)])
    P = 0
+   start = time.time()
    for i in points:
       ls = points[:]
       ls.remove(i)
@@ -124,8 +136,10 @@ def lagrange():
       f2= sympy.simplify(L)
 #      print f2
       P = P + (i[1]*L)
+   end = time.time()
    Pn = sympy.simplify(P)
    print "The legrange Polynomail is: " + str(Pn)
+   print "It took  " + str(end-start) + " seconds to calculate"
    Pu = sympy.lambdify(x, P, 'numpy')
    Pe = i[1]- Pu(i[0])
    print "The error is: " +  str(Pe)
@@ -135,9 +149,11 @@ def lagrange():
 def piecewise():
    n = input('How many points do you want? ')
    points = []
-   for i in np.arange(-5.,5.,(10./(n+1))):
+   for i in np.arange(-5.,5.,(10./(n))):
       points.append([i,1./(1.+i**2.)])
+   points.append([5.,1./(1.+5.**2.)])
    pw = []
+   start = time.time()
    for k in points:
       if 0 == points.index(k):
          j = k
@@ -159,20 +175,26 @@ def piecewise():
    #for i in pw: func.append(sympy.lambdify(x, i[0], 'numpy'))
    #fA= np.piecewise(x,cond,func)
    #print fA
+   end = time.time()
    print "the piecewise function is as follows: "
    print "F(x) = { "
    for i in pw:
       print str(i[0]) + "      for " + str(i[1]) + " < x < " + str(i[2]) 
    print "}"
+   print "It took  " + str(end-start) + " seconds to calculate"
 
 def raised_cosine():
    n = input('How many points do you want? ')
    points = []
-   for i in np.arange(-5.,5.,(10./(n+1))):
+   for i in np.arange(-5.,5.,(10./(n))):
       points.append([i,1./(1.+i**2.)])
+   points.append([5.,1./(1.+5.**2.)])
    pw = []
+   print points
    j = points[0]
    x = sympy.Symbol('x')
+   pw = []
+   start = time.time()
    for k in points:
       if 0 == points.index(k):
          j = k
@@ -181,22 +203,46 @@ def raised_cosine():
          mu = j[0]+(x-j[0])/(k[0]-j[0])
 #mu = x1+(x-x1)/(x2-x1)
          mu2 = (1-sympy.cos(mu*np.pi))/2;
-         print(j[1]*(1-mu2)+k[1]*mu2)
-
-#    def interpolate(a,b,x):
-#        ''' cosine interpolation '''
-#        curve = []
-#        for i in xrange(x):
-#            f = (1-cos(i/float(x)*pi))/2
-#            curve.append((a*(1-f)+b*f))
-#        return curve
+         y = (j[1]*(1-mu2)+k[1]*mu2)
+         pw.append([y,j[0],k[0]])
+         j = k
+   end = time.time()
+   print "the piecewise function is as follows: "
+   print "F(x) = { "
+   for i in pw:
+      print str(i[0]) + "      for " + str(i[1]) + " < x < " + str(i[2]) 
+   print "}"
+   print "It took  " + str(end-start) + " seconds to calculate"
    
+def lsa():
+   n = input('What order least square approximation polynomial do you want?: ')
+   points=[]
+   yvals=[]
+   xvals=[]
+   start = time.time()
+   for i in np.arange(-5.,5.,(10./(n+1))):
+      points.append([i,1./(1.+i**2.)])
+      yvals.append(1.+i**2.)
+      xvals.append(i)
+   points.append([5.,1./(1.+5.**2.)])
+   yvals.append(1./(1.+5.**2.))
+   xvals.append(5.)
+   A = np.vander(xvals, n)
+   # find the x that minimizes the norm of Ax-y
+   (coeffs, residuals, rank, sing_vals) = np.linalg.lstsq(A, yvals)
+   # create a polynomial using coefficients
+   f = np.poly1d(coeffs)
+   end = time.time()
+   print "The least square approximation is: " + str(f)
+   print "It took  " + str(end-start) + " seconds to calculate"
+
 def interpolate():
   while(True):
    print("Please choose one of the following options:")
    print("1. Lagrange")
    print("2. Piecewise")
    print("3. Raised Cosine")
+   print("4. Least Square Approximation")
    print("5. Quit")
    n = input("")
    if(imenu[n]()):
@@ -238,6 +284,7 @@ rmenu = {2:midpoint,
 imenu = {1:lagrange,
          2:piecewise,
          3:raised_cosine,
+         4:lsa,
          5:exit,
          'q':exit}
 
