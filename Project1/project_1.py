621cd8dc1060f1625264fe7f59faa03a2c649cf4
Seth Shelnutt <shelnutt2@gmail.com>
2012-10-22 13:47:06 -0400

    Finished Latex, code needs comments

--- Project1/project_1.py
+++ Project1/project_1.py
@@ -4,6 +4,7 @@
 
 #All code is released under the terms of the gpl v3 or later.
 import argparse, math, sympy, time
+from multiprocessing import Process, Queue, Pool
 import numpy as np
 from matplotlib import pyplot
 from sympy.utilities.lambdify import lambdastr
@@ -11,7 +12,6 @@ import easygui as eg
 
 parser = argparse.ArgumentParser(description='This is a program which demostrates various root finding and interpolation methods.')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.8')
-#parser.add_argument('Rows', metavar='Rows', type=eval, nargs='+', help='Rows of the matrix')
 
 
 #initialize the arguments
@@ -79,6 +79,30 @@ def newtons():
    print("The zero is: " + str(xk))
    print("It took " + str(i) + " interations and " + str(end-start) + " seconds.")
 
+def secant():
+   b = input('Please enter the first starting value: ')
+   a = input('Please enter the second starting value: ')
+   b = np.float(b)
+   a = np.float(a)
+   x = sympy.Symbol('x')
+   start = time.time()
+   y = sympy.exp((-x/5)) - sympy.sin(x)
+   f = sympy.lambdify(x, y, 'numpy')
+   found = False
+   i = 0
+   while(found == False):
+      i = i+1
+      xk = a - (f(a)*(a-b))/(f(a)-f(b))
+      if abs(f(xk))<10**-7:
+         print("Done")
+         found = True
+      else:
+         b = a
+         a = xk
+   end = time.time()
+   print("The zero is: " + str(xk))
+   print("It took " + str(i) + " interations and " + str(end-start) + " seconds.")
+
 def visual():
    def onclick(event):
     msg = "Do you want to choose x=%f"%(event.xdata)
@@ -96,18 +120,12 @@ def visual():
    x1 = np.arange(a,b,.01)
    y1 = np.sin(x1)
    y2 = np.exp(-x1/5)
-
    fig = pyplot.figure()
    ax = fig.add_subplot(111)
-
    ax.plot( x1,y1 )
    ax.plot( x1,y2 )
-
    ax.axis([a, b, -1.5, 1.5])
-
    cid = fig.canvas.mpl_connect('button_press_event', onclick)
-
-
    pyplot.title( 'Plotting sin(x) and e^(-x/5)' )
    pyplot.xlabel( 'X Axis' )
    pyplot.ylabel( 'Y Axis' )
@@ -134,9 +152,7 @@ def lagrange():
          x = sympy.Symbol('x')
          y = (x-k[0])/(i[0]-k[0])
          L = L * y
-#      print f
       f2= sympy.simplify(L)
-#      print f2
       P = P + (i[1]*L)
    end = time.time()
    Pn = sympy.simplify(P)
@@ -144,6 +160,54 @@ def lagrange():
    print "It took  " + str(end-start) + " seconds to calculate"
    Pu = sympy.lambdify(x, P, 'numpy')
    Pe = i[1]- Pu(i[0])
+   print "The error is: " +  str(Pe)
+   #yprime = y.diff(x)
+   #fprime = sympy.lambdify(x, yprime, 'numpy')
+   x = np.arange(-5.,5.,(10./(n)))
+   xlot =  np.linspace(-5,5,1000)
+   y = Pu(xlot)
+   z = 1/(1+xlot**2)
+   zpoint = 1/(1+x**2)
+   pyplot.plot(xlot,y,label="Lagrange Polynomial")
+   pyplot.plot(x,zpoint,'co')
+   pyplot.plot(xlot,z,label="1/(1+x^2)")
+   pyplot.legend()
+   pyplot.ylim(0,2)
+   pyplot.title(str(n)+"th Degree Lagrange Polynomial")
+   pyplot.show()
+
+def lform(i):
+      ls = data[:]
+      ls.remove(i)
+      L = 1
+      for k in ls:
+         x = sympy.Symbol('x')
+         y = (x-k[0])/(i[0]-k[0])
+         L = L * y
+      return(i[0]*L)
+
+def lagrange2():
+   n = input('What order lagrange polynomial do you want?: ')
+   points=[]
+   for i in np.arange(-5.,5.,(10./(n))):
+      points.append([i,1./(1.+i**2.)])
+   points.append([5.,1./(1.+5.**2.)])
+   global data
+   data = points[:]
+   P = 0
+   start = time.time()
+   queue1 = Queue() #create a queue object
+   procs = []
+   pool = Pool(processes=24)              # start 24 worker processes
+   result = pool.map(lform, points)
+   x = sympy.Symbol('x')
+   P = sum(result)
+   end = time.time()
+   Pn = sympy.simplify(P)
+   print "The lagrange Polynomail is: " + str(Pn)
+   print "It took  " + str(end-start) + " seconds to calculate"
+   Pu = sympy.lambdify(x, P, 'numpy')
+   Pe = points[len(points)-1][1]- Pu(points[len(points)-1][0])
    print "The error is: " +  str(Pe)
    #yprime = y.diff(x)
    #fprime = sympy.lambdify(x, yprime, 'numpy')
@@ -213,7 +277,6 @@ def raised_cosine():
       points.append([i,1./(1.+i**2.)])
    points.append([5.,1./(1.+5.**2.)])
    pw = []
-   print points
    j = points[0]
    x = sympy.Symbol('x')
    pw = []
@@ -221,17 +284,19 @@ def raised_cosine():
    for k in points:
       if 0 == points.index(k):
          j = k
-      else:
-#           t = (1 - cos(t*PI))/2;
-#	    return  a*(1-t) + b*t;
-#         c = .5*(1+np.cosine(x))
+      else: 
          mu = j[0]+(x-j[0])/(k[0]-j[0])
-#mu = x1+(x-x1)/(x2-x1)
-         mu2 = (1-sympy.cos(mu*np.pi))/2;
+         mu2 = (1-sympy.cos(mu*np.pi))/2
          y = (j[1]*(1-mu2)+k[1]*mu2)
          pw.append([y,j[0],k[0]])
          j = k
    end = time.time()
+   for i in range(len(pw)):
+    if i%2==0:
+        print pw[i][0]
+        W = sympy.lambdify(x,pw[i][0],'numpy')
+        pw[i][0]  = -(pw[i][0].as_leading_term(x) - W(.5))*sympy.cos(np.pi*x)+W(.5)
+        print pw[i][0]
    print "the piecewise function is as follows: "
    print "F(x) = { "
    for i in pw:
@@ -240,8 +305,8 @@ def raised_cosine():
    print "It took  " + str(end-start) + " seconds to calculate"
    
    x = sympy.Symbol('x')
-   y = sympy.lambdify(x,i[0],'numpy')
    for i in pw:
+      y = sympy.lambdify(x,i[0],'numpy')
       xlot =  np.linspace(i[1],i[2],1000)
       pyplot.plot(xlot,y(xlot))
    xlot2 =  np.linspace(-5,5,1000)
@@ -299,7 +364,8 @@ def interpolate():
    print("2. Piecewise")
    print("3. Raised Cosine")
    print("4. Least Square Approximation")
-   print("5. Quit")
+   print("5. Lagrange Optimized")
+   print("6. Quit")
    n = input("")
    if(imenu[n]()):
       break
@@ -313,7 +379,8 @@ def root():
    print("1. Visual Inspection")
    print("2. Bisection")
    print("3. Newtons")
-   print("4. Quit")
+   print("4. Secant (Newton Optimized)")
+   print("5. Quit")
    n = input("")
    if(rmenu[n]()):
       break
@@ -335,14 +402,15 @@ mmenu = {1:root,
 rmenu = {2:midpoint,
          3:newtons,
          1:visual,
+         4:secant,
          'q':exit,
-         4:exit}
+         5:exit}
 imenu = {1:lagrange,
          2:piecewise,
          3:raised_cosine,
          4:lsa,
-         5:exit,
+         5:lagrange2,
+         6:exit,
          'q':exit}
-
 main_menu()
 
